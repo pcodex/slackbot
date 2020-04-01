7d7b8918b715ab14a37c87e787fdcff28b4c4254
Prab <prab.missier@gmail.com>
2020-04-02 00:20:07 +1100

    Updated behavior and config tokens

---
+++ config/key_prod.js
@@ -0,0 +1,3 @@
+module.exports = {
+    slacktoken:process.env.SLACKTOKEN    
+}
---
+++ index.js
@@ -0,0 +1,41 @@
+var Slackbot = require('slackbots');
+
+const mykeys = require('./config/key');
+
+var bot = new Slackbot({
+    token:mykeys.slacktoken,
+    name:'kuska'
+});
+
+bot.on('start', function(){
+
+    var params={
+        icon_emoji:':mask:'
+    };
+
+    bot.postMessageToChannel('general','Stay indoors', params);
+    bot.getUsers()
+    .then((users)=>{
+        //console.log(users);
+        for(var i=0; i<users.members.length;++i){
+            console.log(users.members[i].id);
+            console.log(users.members[i].name);
+        }
+    });
+
+    bot.postMessageToUser('prabhu', 'Hi, Wear a mask when outdoors',params);
+
+});
+
+bot.on('message', (msg)=>{
+
+    switch(msg.type){
+        case "message":
+            if(msg.channel[0] === 'D' && msg.bot_id === undefined) {
+                bot.postMessage(msg.user, "Visit https://www.dhhs.vic.gov.au/coronavirus-covid-19-daily-update", {as_user:true}) 
+                console.log(msg.user);               
+            }
+            break;
+    }
+
+});
